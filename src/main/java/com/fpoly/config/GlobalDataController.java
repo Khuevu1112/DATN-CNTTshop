c60327edfb316965cd6e5b603d88ac7b674e8f99
nhatbuoitoi <nhatnnd364@gmail.com>
2026-06-22 11:12:15 +0700

    Sửa link đơn hàng admin, làm quản lý giỏ hàng admin

--- src/main/java/com/fpoly/config/GlobalDataController.java
+++ src/main/java/com/fpoly/config/GlobalDataController.java
@@ -1,13 +1,16 @@
 package com.fpoly.config;
 
-import java.util.List;
+import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import java.util.List;
+
 import com.fpoly.model.Category;
 import com.fpoly.repository.CategoryRepository;
+import com.fpoly.service.CartService;
 
 @ControllerAdvice
 public class GlobalDataController {
@@ -15,8 +18,23 @@ public class GlobalDataController {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Autowired
+    private CartService cartService;
+
     @ModelAttribute("menuCategories")
     public List<Category> menuCategories() {
         return categoryRepository.findAllByOrderBySortOrderAsc();
+    }
+
+    @ModelAttribute("cartItemCount")
+    public int cartItemCount(Principal principal) {
+        if (principal == null) {
+            return 0;
+        }
+        try {
+            return cartService.demSoLuongItem(principal.getName());
+        } catch (RuntimeException e) {
+            return 0;
+        }
     }
 }
